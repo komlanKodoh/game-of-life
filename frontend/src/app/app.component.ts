acfaa95b16666922d75875b278aedda9cb275d4e
Behemoth11 <kodohdaniel@gmail.com>
2022-04-19 16:58:21 -0500

    - connected engine and front end

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { greet } from 'game-of-life-engine';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
+
 export class AppComponent {
   title = 'frontend';
+
+  constructor(){
+    greet();
+  }
 }
+
