9d730c3dbce60c5dfd13b049be38d2b9f82deff9
Behemoth11 <kodohdaniel@gmail.com>
2022-04-22 22:27:17 -0500

    - minor : added modulo to the function that adds cells

--- engine-interface/src/lib/ecosystem.ts
+++ engine-interface/src/lib/ecosystem.ts
@@ -1,4 +1,3 @@
-
 import Directive from './Configuration/directive';
 import Cell from './cell';
 
@@ -6,50 +5,61 @@ export default class Ecosystem {
   /**
    * A list of all state live/death present in the current ecosystem;
    */
-   state: Uint8Array;
+  state: Uint8Array;
 
-  constructor(private width: number, private height: number) {
-    this.state = new Uint8Array(width * height);
+  constructor(private rows: number, private columns: number) {
+    this.state = new Uint8Array(rows * columns);
   }
 
-  set_width(width: number) {
-    this.width = width;
+  set_rows(rows: number) {
+    this.rows = rows;
   }
 
-  set_height(height: number) {
-    this.height
-    this.state
-    this.height = height;
+  set_columns(columns: number) {
+    this.columns = columns;
+  }
+
+  
+  add(directive: Directive) {
+    const parser = new Directive.Parser();
+    parser.feed(directive);
+    
+    for (;;) {
+      const cell = parser.next_cell();
+      
+      if (cell === null) {
+        break;
+      }
+      
+      this.bless(cell);
+    }
   }
 
   /**
    * Returns a cell index in the linear world;
    */
-  get_cell_index([row, column]: Cell) {
-    return row * this.width + column;
-  }
-
-  add(directive: Directive) {
-    const parser = new Directive.Parser();
-    parser.feed ( directive )
-
-    for (;;) {
-      const cell = parser.next_cell();
-
-      if (cell === null ) {
-        break;
-      }
-
-      this.bless ( cell )
-    }
-
-  }
-
-  bless( cell: Cell){
-    let idx = this.get_cell_index( cell );
+  private get_cell_index([_row, _column]: Cell) {
     
-    this.state.set ( [255], idx )
+    let row = _row % this.rows;
+    let column = _column % this.columns;
+
+    return row * this.rows + column;
   }
 
 
+  /**
+   * Brings a cell back to live
+   */
+  bless(cell: Cell) {
+    let idx = this.get_cell_index(cell);
+
+    this.state.set([255], idx);
+  }
+
+  /**
+   * 
+   */
+  kill(_cell: Cell){
+    
+  }
 }
