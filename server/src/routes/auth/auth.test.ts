8fcd7ed878e7ff52ef1bfc6f58fc372e28cabc2a
Behemoth11 <kodohdaniel@gmail.com>
2022-05-06 23:59:23 -0500

    - added : jest unit testing

--- server/src/routes/auth/auth.test.ts
+++ server/src/routes/auth/auth.test.ts
@@ -1,2 +1,17 @@
 
-describe()
+import { getToken, parseToken } from "./auth.service";
+
+describe("Authentication service test", () => {
+  it("Should create token", () => {
+    let token = getToken({ id: "1aoga-agag-aga" });
+    expect(token).toBeDefined();
+  });
+
+  it("Should parse token", () => {
+      let token  =getToken({id: "ID"});
+      let userIdentifier = parseToken(token);
+
+      expect(userIdentifier).toBeDefined();
+      expect(userIdentifier.id).toBeDefined();
+  });
+});
