5704a758046c5114535eb89f3c868f3ae3dbd76f
Behemoth11 <kodohdaniel@gmail.com>
2022-05-24 15:01:02 -0500

    - fixed : missing build step in bash script

--- _build.sh
+++ _build.sh
@@ -2,8 +2,8 @@
 
 cd "$(dirname "$0")"
 
-# ./server/_build.sh &&
-# ./engine-interface/_build.sh &&
-# ./frontend/_build.sh
+./server/_build.sh &&
+./engine-interface/_build.sh &&
+./frontend/_build.sh
 
 docker build . -t game-of-life
